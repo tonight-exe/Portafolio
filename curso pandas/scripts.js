da9c484ddbcfdad5c3d45da17e2b5e40f339252d
Tonight <gonzalo.gomez.agg@gmail.com>
2024-09-15 20:57:06 -0300

    Add files via upload
    hecho darkmode light mode

---
+++ curso pandas/scripts.js
@@ -0,0 +1,14 @@
+const toggletheme = document.getElementById("toggle-theme");
+const toggleicon = document.getElementById("toggle-icon");
+const toggletext = document.getElementById("toggle-text");
+
+toggletheme.addEventListener("click", ()=>{
+    document.body.classList.toggle("dark");
+    if(toggleicon.src.includes("moon.svg")){
+        toggleicon.src = "assets/icons/sun.svg";
+        toggletext.textContent = "light mode";
+    }else{
+        toggleicon.src = "assets/icons/moon.svg";
+        toggletext.textContent = "dark mode";
+    }
+})
